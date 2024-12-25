92134710e623a26e873ce8302cd94933c47ec0ac
Andrew Meshkov <meshkov89@gmail.com>
2024-12-25 15:16:35 +0300

    верстка фильтров для магазина

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,7 +2,23 @@ const Filter = () => {
   return (
     <div className="widget">
       <h2 className="widget__title">Фильтр</h2>
-      <div className="widget__body">Фильтр</div>
+      <div className="widget__body">
+        {/* Дополнительные фильтры */}
+        <div className="filter filter-price">
+          <div className="filter__title">Цена</div>
+          <div className="filter__body"></div>
+        </div>
+        {/* Дополнительные фильтры */}
+        <div className="filter filter-memory">
+          <div className="filter__title">Объем памяти</div>
+          <div className="filter__body"></div>
+        </div>
+        {/* Дополнительные фильтры */}
+        <div className="filter filter-color">
+          <div className="filter__title">Цвета</div>
+          <div className="filter__body"></div>
+        </div>
+      </div>
     </div>
   );
 };
